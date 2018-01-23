58b6f7a423de629ce484b03da211197c679717f5
Tash Postolovski <npostolovski@gmail.com>
2018-01-23 20:34:00 +1100

    create players from spreadsheet rows

--- app/helpers/import.rb
+++ app/helpers/import.rb
@@ -13,6 +13,12 @@ class Import
     xlsx = Roo::Spreadsheet.open(filepath)
     club_name = xlsx.sheet(0).row(1)[0]
 
+    xlsx.each_row_streaming(offset: 2) do |row|
+      name = row[0].to_s.split
+
+      Player.create(first_name: name[0], last_name: name[1])
+    end
+
     Club.create(name: club_name)
   end
 end
--- spec/helpers/import_spec.rb
+++ spec/helpers/import_spec.rb
@@ -1,12 +1,21 @@
 describe Import do
+  let(:leamington_filename) { "test_club.xlsx" }
+  let(:import) { Import.new(leamington_filename) }
+
   it "creates a club from the xlsx file" do
-    leamington_filename = "test_club.xlsx"
     expected_club_name = "Leamington FC"
 
-    import = Import.new(leamington_filename)
     import.club
 
     expect(Club.all.count).to eq 1
     expect(Club.first.name).to eq expected_club_name
+  end
+
+  it "creates the correct number of players" do
+    expected_number_of_players = 18
+
+    import.club
+
+    expect(Player.all.count).to eq expected_number_of_players
   end
 end
