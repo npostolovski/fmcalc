712ddf67cbe3ce69c37b3779342989ae27877b13
Tash Postolovski <npostolovski@gmail.com>
2018-01-25 14:23:33 +1100

    fix test to use correct player attributes

--- spec/helpers/suitability_spec.rb
+++ spec/helpers/suitability_spec.rb
@@ -5,44 +5,44 @@ describe Suitability do
     one_on_ones: 7,
     acceleration: 11,
     aerial_ability: 9,
-    agility: 8,
-    anticipation: 12,
-    bravery: 10,
+    agility: 12,
+    anticipation: 10,
+    bravery: 7,
     balance: 9,
-    command_of_area: 7,
-    composure: 6,
-    communication: 12,
-    corners: 6,
-    concentration: 3,
-    crossing: 9,
-    decisions: 1,
-    determination: 10,
-    dribbling: 12,
-    eccentricity: 2,
-    finishing: 11,
-    first_touch: 2,
-    flair: 4,
-    free_kicks: 3,
-    handling: 5,
-    heading: 10,
-    jumping_reach: 1,
+    command_of_area: 6,
+    composure: 12,
+    communication: 6,
+    corners: 3,
+    concentration: 9,
+    crossing: 1,
+    decisions: 10,
+    determination: 12,
+    dribbling: 2,
+    eccentricity: 11,
+    finishing: 2,
+    first_touch: 4,
+    flair: 3,
+    free_kicks: 5,
+    handling: 10,
+    heading: 1,
+    jumping_reach: 9,
     kicking: 9,
-    leadership: 9,
-    long_shots: 4,
+    leadership: 4,
+    long_shots: 1,
     long_throws: 1,
-    marking: 1,
-    natural_fitness: 2,
-    off_the_ball: 8,
-    passing: 1,
-    pace: 7,
-    positioning: 14,
-    punching: 13,
-    reflexes: 10,
-    rushing_out: 9,
-    stamina: 10,
-    strength: 8,
-    tackling: 6,
-    teamwork: 1,
+    marking: 2,
+    natural_fitness: 8,
+    off_the_ball: 1,
+    passing: 7,
+    pace: 14,
+    positioning: 13,
+    punching: 10,
+    reflexes: 9,
+    rushing_out: 10,
+    stamina: 8,
+    strength: 6,
+    tackling: 1,
+    teamwork: 9,
     technique: 5,
     throwing: 7,
     vision: 8,
@@ -93,56 +93,56 @@ describe Suitability do
     end
 
     it "returns the expected primary score for player and role" do
-      expected_primary_score = 75
+      expected_primary_score = 83.00
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.primary_score).to eq expected_primary_score
     end
 
     it "returns the expected secondary score for player and role" do
-      expected_secondary_score = 47
+      expected_secondary_score = 56.00
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.secondary_score).to eq expected_secondary_score
     end
 
     it "returns the expected total score" do
-      expected_total_score = 98.5
+      expected_total_score = 111.00
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.total_score).to eq expected_total_score
     end
 
     it "returns the expected average primary score" do
-      expected_average_primary_score = 8.33
+      expected_average_primary_score = 9.22
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.average_primary).to eq expected_average_primary_score
     end
 
     it "returns the expected average secondary score" do
-      expected_average_secondary_score = 6.71
+      expected_average_secondary_score = 8.00
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.average_secondary).to eq expected_average_secondary_score
     end
 
     it "returns the expected total average" do
-      expected_total_average = 7.52
+      expected_total_average = 8.61
       suitability_score = Suitability.new(player, role).determine
 
       expect(suitability_score.total_average).to eq expected_total_average
     end
 
     it "returns a percentage_primary score" do
-      expected_percentage_primary = 41.67
+      expected_percentage_primary = 46.11
 
       suitability_score = Suitability.new(player, role).determine
       expect(suitability_score.percentage_primary).to eq expected_percentage_primary
     end
 
     it "returns a percentage_secondary score" do
-      expected_percentage_secondary = 33.57
+      expected_percentage_secondary = 40.00
 
       suitability_score = Suitability.new(player, role).determine
       expect(suitability_score.percentage_secondary).to eq expected_percentage_secondary
