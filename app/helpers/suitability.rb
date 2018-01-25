f36c9e8c384c363a7c408f6e8531903e3d56c317
Tash Postolovski <npostolovski@gmail.com>
2018-01-25 11:00:58 +1100

    refactor suitability class

--- app/helpers/suitability.rb
+++ app/helpers/suitability.rb
@@ -1,4 +1,6 @@
 class Suitability
+  SECONDARY_ATTRIBUTES_MODIFIER = 0.5
+
   attr_accessor :player, :role
 
   def initialize(player, role)
@@ -7,19 +9,27 @@ class Suitability
   end
 
   def determine
-    primary_attributes_for_player = role.primary_attributes.map do |attribute|
-      player.abilities.send(attribute)
-    end
-
-    secondary_attributes_for_player = role.secondary_attributes.map do |attribute|
-      player.abilities.send(attribute)
-    end
-
     SuitabilityScore.create(
       player: player,
       role: role,
-      primary_score: primary_attributes_for_player.sum,
-      secondary_score: secondary_attributes_for_player.sum / 2
+      primary_score: relevant_attribute_score(:primary_attributes),
+      secondary_score: relevant_attribute_score(:secondary_attributes)
     )
+  end
+
+  private
+
+  def relevant_attribute_score(primary_or_secondary_attributes)
+    scores = calculate_scores(primary_or_secondary_attributes)
+    if primary_or_secondary_attributes == :secondary_attributes
+      scores = scores * SECONDARY_ATTRIBUTES_MODIFIER
+    end
+    scores
+  end
+
+  def calculate_scores(primary_or_secondary_attributes)
+    role.send(primary_or_secondary_attributes).inject(0) do |sum, attr|
+      player.abilities.send(attr)
+    end
   end
 end
